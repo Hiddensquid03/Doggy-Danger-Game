8c8bf0671d44a2966579d642748f94ce3051594d
Hidden <137702933+Hiddensquid03@users.noreply.github.com>
2025-01-22 11:26:49 +0000

    Added movement set up template for character

--- player.gd
+++ player.gd
@@ -1,3 +1,58 @@
 extends Area2D
 
 @export var speed = 400 # pixels p sec
+var screen_size
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("Move_Right1"):
+		velocity.x += 1
+	if Input.is_action_pressed("Move_Left1"):
+		velocity.x -= 1
+	if Input.is_action_pressed("Move_Down1"):
+		velocity.y += 1
+	if Input.is_action_pressed("Move_Up1"):
+		velocity.y -= 1
+	
+	if Input.is_action_pressed("Move_Right2"):
+		velocity.x += 1
+	if Input.is_action_pressed("Move_Left2"):
+		velocity.x -= 1
+	if Input.is_action_pressed("Move_Down2"):
+		velocity.y += 1
+	if Input.is_action_pressed("Move_Up2"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "Walk_Right"
+		$AnimatedSprite2D.flip_v = false
+		# See the note below about the following boolean assignment.
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "Walk"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+		
+		
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
+		
+signal hit
+
+
+
+func _on_body_entered(body):
+	pass # Replace with function body.
